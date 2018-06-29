558d2dd807242f173780f82a1bf77c0420012d7b
Ben Williams <subsy@users.noreply.github.com>
2018-06-29 18:49:45 +0100

    ANALYTICS-2053
    Fix for Failing Echo integration

--- src/main/java/loki/App.java
+++ src/main/java/loki/App.java
@@ -8,8 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        //insert bad code here      
+        System.out.println( "Hello World!" );    
+        
         
     }
 }
